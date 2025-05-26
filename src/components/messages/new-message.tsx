23e482a6151ad5383037c0eaafa85fb32a6f1ca5
ArvindParekh <aruparekh@gmail.com>
2025-05-26 12:46:06 -0400

    feat(messages): send messages through enter

--- src/components/messages/new-message.tsx
+++ src/components/messages/new-message.tsx
@@ -1,62 +1,78 @@
 "use client";
-
 import { Send } from "lucide-react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { useEffect, useState } from "react";
 import { getWsClient } from "@/lib/wsClient";
 
-export default function NewMessage({userId, chatId, receiverId}: {userId: string, chatId: string, receiverId: string}) {
-    const [message, setMessage] = useState("");
-    const [socket, setSocket] = useState<WebSocket | null>(null);
+export default function NewMessage({
+   userId,
+   chatId,
+   receiverId,
+}: {
+   userId: string;
+   chatId: string;
+   receiverId: string;
+}) {
+   const [message, setMessage] = useState("");
+   const [socket, setSocket] = useState<WebSocket | null>(null);
 
-    useEffect(()=> {
+   useEffect(() => {
       const socket = getWsClient();
       setSocket(socket);
-      
-    }, []);
+   }, []);
 
-    const handleSendMessage = (userId: string) => {
-        console.log("Sending message: ", {
-          type: "message",
-          content: message,
-          senderId: userId,
-          receiverId: receiverId,
-          chatId: chatId,
-        });
+   const handleSendMessage = (userId: string) => {
+      if (!message.trim()) return;
 
-        try {
-          socket?.send(JSON.stringify({
-            type: "message",
-            content: message,
-            senderId: userId,
-            receiverId: receiverId,
-            chatId: chatId,
-          }))
+      console.log("Sending message: ", {
+         type: "message",
+         content: message,
+         senderId: userId,
+         receiverId: receiverId,
+         chatId: chatId,
+      });
 
-          setMessage("");
-        } catch (error) {
-          console.error("Error sending message: ", error);
-        }
-    }
-    
+      try {
+         socket?.send(
+            JSON.stringify({
+               type: "message",
+               content: message,
+               senderId: userId,
+               receiverId: receiverId,
+               chatId: chatId,
+            })
+         );
+         setMessage("");
+      } catch (error) {
+         console.error("Error sending message: ", error);
+      }
+   };
 
-    return (
-        <div className="flex-grow relative">
-          <Input
-            placeholder="Type a message..."
-            className="pr-10 border-pink-500/20 focus-visible:ring-pink-500"
+   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+         e.preventDefault(); 
+         handleSendMessage(userId);
+      }
+   };
+
+   return (
+      <div className='flex-grow relative'>
+         <Input
+            placeholder='Type a message...'
+            className='pr-10 border-pink-500/20 focus-visible:ring-pink-500'
             value={message}
-            onChange={(e)=> setMessage(e.target.value)}
-          />
-          <Button
-            size="icon"
-            className="absolute right-1 top-1 h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-yellow-400 text-black hover:opacity-90"
-            onClick={()=> handleSendMessage(userId as string)}
-          >
-            <Send className="h-3 w-3" />
-            <span className="sr-only">Send</span>
-          </Button>
-        </div>
-    )
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown}
+         />
+         <Button
+            size='icon'
+            className='absolute right-1 top-1 h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-yellow-400 text-black hover:opacity-90'
+            onClick={() => handleSendMessage(userId as string)}
+         >
+            <Send className='h-3 w-3' />
+            <span className='sr-only'>Send</span>
+         </Button>
+      </div>
+   );
 }
