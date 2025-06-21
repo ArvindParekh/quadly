d932041ae878e7549c4228211d0466842d6d9eb5
ArvindParekh <aruparekh@gmail.com>
2025-06-21 12:35:16 -0400

    chore(prisma): remove generated wasm.js file and update environment path references in edge.js and index.js

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -363,7 +363,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
